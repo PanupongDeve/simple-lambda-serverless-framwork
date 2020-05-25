cacd4f46db64fdc3df9009ce940736b0fcd721d1
Panupong Chamsomboon <5635512110@psu.ac.th>
2020-05-25 23:08:54 +0000

    add cloudwatch tail

--- hello-serverless/add.ts
+++ hello-serverless/add.ts
@@ -7,15 +7,17 @@ export const add: APIGatewayProxyHandler = async (event, _context) => {
         num2
     } = JSON.parse(event.body);
 
+    const output = {
+        num1,
+        num2,
+        result: num1 + num2,
+        query: event.queryStringParameters,
+        multiQuery: event.multiValueQueryStringParameters
+      }
 
+      console.log(output);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      num1,
-      num2,
-      result: num1 + num2,
-      query: event.queryStringParameters,
-      multiQuery: event.multiValueQueryStringParameters
-    }, null, 2),
+    body: JSON.stringify(output, null, 2),
   };
 }
